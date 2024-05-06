9c6f085d1cdafb78689dcd39c18cb99987dac7f1
Alex Brewster <alexgbrewster@gmail.com>
2024-05-05 23:53:45 -0400

    shadcn components

--- components/models/storeModel.tsx
+++ components/models/storeModel.tsx
@@ -1,10 +1,20 @@
 "use client";
 
 import { useStoreModel } from "@/hooks/useStoreModel";
-import { Model } from "../ui/model";
+import { Model } from "@/components/ui/model";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 const formSchema = z.object({
   name: z.string().min(1),
 });
@@ -13,7 +23,13 @@ export function StoreModel() {
   const storeModel = useStoreModel();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
   });
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
   return (
     <Model
       title="Create Store"
@@ -21,7 +37,31 @@ export function StoreModel() {
       isOpen={storeModel.isOpen}
       onClose={storeModel.onClose}
     >
-      Future Create Store Form
+      <div className="space-y-4 py-2 pb-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="E-commerce" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="pt-6 space-x-2 flex items-center justify-end">
+              <Button variant="outline" onClick={storeModel.onClose}>
+                Cancel
+              </Button>
+              <Button type="submit">Continue</Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </Model>
   );
 }

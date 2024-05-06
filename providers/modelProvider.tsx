5cbfd546954f313a8d49168016449a8fc635af67
Alex Brewster <alexgbrewster@gmail.com>
2024-05-05 23:26:53 -0400

    create store model

--- providers/modelProvider.tsx
+++ providers/modelProvider.tsx
@@ -1,17 +1,19 @@
 "use client";
 
-import { StoreModal } from "@/components/modals/store-modal";
+import { StoreModel } from "@/components/models/storeModel";
 import { useEffect, useState } from "react";
 
-export function ModalProvider() {
+export function ModelProvider() {
   const [isMounted, setIsMounted] = useState(false);
-
   useEffect(() => {
     setIsMounted(true);
   }, []);
-
   if (!isMounted) {
     return null;
   }
-  return <StoreModal />;
+  return (
+    <>
+      <StoreModel />
+    </>
+  );
 }
